08e494f4ea54bba5d454146648c0d1b9be11a4c6
katieyungchung <katieyungchung@g.ucla.edu>
2025-03-07 03:22:57 -0800

    Modify bottom tabs and add search bar

--- Sanctuary-frontend/app/(tabs)/_layout.tsx
+++ Sanctuary-frontend/app/(tabs)/_layout.tsx
@@ -1,48 +1,161 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Tabs as ExpoTabs, Tabs } from "expo-router";
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import Map from './map';
-import ReportForm from './report-form';
-import SafeWalk from './safe-walk';
-import Profile from './profile';
+// import React from 'react';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { Tabs as ExpoTabs, Tabs } from "expo-router";
+// import Icon from 'react-native-vector-icons/MaterialIcons';
+// import Map from './map';
+// import ReportForm from './report-form';
+// import SafeWalk from './safe-walk';
+// import Profile from './profile';
+// import { Ionicons } from "@expo/vector-icons"; // Import icons
 
-const Tab = createBottomTabNavigator();
+// const Tab = createBottomTabNavigator();
+
+
+// export default function TabLayout() {
+//   return (
+//     <Tab.Navigator id={undefined}
+//       screenOptions={({ route }) => ({
+//         headerShown: false,
+//         // tabBarIcon: ({ color, size }) => {
+//         //   let iconName;
+
+//         //   if (route.name === 'map') {
+//         //     iconName = 'map';
+//         //   } else if (route.name === 'report-form') {
+//         //     iconName = 'report';
+//         //   } else if (route.name === 'safe-walk') {
+//         //     iconName = 'directions-walk';
+//         //   } else if (roaute.name === 'profile') {
+//         //     iconName = 'person-outline';
+//         //   }
+
+//         //   // You can return any component that you like here!
+//         //   return <Icon name={iconName} size={size} color={color} />;
+//         // },
+//         tabBarActiveTintColor: '#F2E9D8',
+//         tabBarInactiveTintColor: '#B0ABA2',
+//         tabBarStyle: {
+//           backgroundColor: '#184040',
+//           borderTopWidth: 0,
+//           elevation: 5,
+//         },
+//       })}
+//     >
+//       <Tab.Screen name="map" component={Map} options={{ title: "Incident Map" }} />
+//       <Tab.Screen name="report-form" component={ReportForm} options={{ title: "Report Form" }} />
+//       <Tab.Screen name="safe-walk" component={SafeWalk} options={{ title: "Safe Walk" }} />
+//       <Tab.Screen name="profile" component={Profile} options={{ title: "Profile" }} />
+//     </Tab.Navigator>
+//   );
+// }
+
+// // import { Tabs } from "expo-router";
+// // import { Ionicons } from "@expo/vector-icons"; // Import icons
+
+
+// // export default () => {
+// //     return (
+// //         <Tabs
+// //             screenOptions={{
+// //                 tabBarStyle: {
+// //                     backgroundColor: '#184040',
+// //                     height: 100,
+// //                     paddingBottom: 50,
+// //                     paddingTop: 10,
+// //                 },
+// //                 tabBarActiveTintColor: '#F2E9D8',
+// //                 tabBarInactiveTintColor: '#B0ABA2',
+// //                 headerShown: false,
+// //                 // tabBarShowLabel: false,
+// //             }}>
+// //             <Tabs.Screen 
+// //                 name="home" 
+// //                 options={{
+// //                     tabBarIcon: ({color, size}) => (
+// //                         <Ionicons name="location-outline" size={30} color={color} style={{ justifyContent: 'center', height: '100%' }}/>
+// //                     ),
+// //                 }}
+// //             />
+// //              <Tabs.Screen 
+// //                 name="report" 
+// //                 options={{
+// //                     tabBarIcon: ({color, size}) => (
+// //                         <Ionicons name="megaphone-outline" size={30} color={color} />
+// //                     ),
+// //                 }}
+// //             />
+// //             <Tabs.Screen 
+// //                 name="safewalk" 
+// //                 options={{
+// //                     tabBarIcon: ({color, size}) => (
+// //                         <Ionicons name="walk-outline" size={30} color={color} />
+// //                     ),
+// //                 }}
+// //             />
+           
+// //         </Tabs>
+// //     );
+// // };
+
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons"; // Import icons
 
 export default function TabLayout() {
-  return (
-    <Tab.Navigator id={undefined}
-      screenOptions={({ route }) => ({
-        headerShown: false,
-        tabBarIcon: ({ color, size }) => {
-          let iconName;
-
-          if (route.name === 'map') {
-            iconName = 'map';
-          } else if (route.name === 'report-form') {
-            iconName = 'report';
-          } else if (route.name === 'safe-walk') {
-            iconName = 'directions-walk';
-          } else if (route.name === 'profile') {
-            iconName = 'person';
-          }
-
-          // You can return any component that you like here!
-          return <Icon name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: '#2f95dc',
-        tabBarInactiveTintColor: '#2C6868',
-        tabBarStyle: {
-          backgroundColor: '#184040',
-          borderTopWidth: 0,
-          elevation: 5,
-        },
-      })}
-    >
-      <Tab.Screen name="map" component={Map} options={{ title: "Incident Map" }} />
-      <Tab.Screen name="report-form" component={ReportForm} options={{ title: "Report Form" }} />
-      <Tab.Screen name="safe-walk" component={SafeWalk} options={{ title: "Safe Walk" }} />
-      <Tab.Screen name="profile" component={Profile} options={{ title: "Profile" }} />
-    </Tab.Navigator>
-  );
-}
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarStyle: {
+                    backgroundColor: '#184040',
+                    height: 100,
+                    paddingBottom: 50,
+                    paddingTop: 10,
+                },
+                tabBarActiveTintColor: '#F2E9D8',
+                tabBarInactiveTintColor: '#B0ABA2',
+                headerShown: false,
+                tabBarLabelStyle: {
+                  marginTop: 5, // Adjust the margin as needed
+              },
+                // tabBarShowLabel: false,
+            }}>
+            <Tabs.Screen 
+                name="map" 
+                options={{
+                    title: "Map",
+                    tabBarIcon: ({color, size}) => (
+                        <Ionicons name="location-outline" size={30} color={color} style={{ justifyContent: 'center', height: '100%' }}/>
+                    ),
+                }}
+            />
+             <Tabs.Screen 
+                name="report-form" 
+                
+                options={{
+                    title: "Report",
+                    tabBarIcon: ({color, size}) => (
+                        <Ionicons name="megaphone-outline" size={30} color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen 
+                name="safe-walk" 
+                options={{
+                    title: "Safe Walk",
+                    tabBarIcon: ({color, size}) => (
+                        <Ionicons name="walk-outline" size={30} color={color} />
+                    ),
+                }}
+            />
+             <Tabs.Screen 
+                name="profile" 
+                options={{
+                    title: "Profile",
+                    tabBarIcon: ({color, size}) => (
+                        <Ionicons name="person-outline" size={30} color={color} />
+                    ),
+                }}
+            />
+           
+        </Tabs>
+    );
+};
--- Sanctuary-frontend/app/(tabs)/map.tsx
+++ Sanctuary-frontend/app/(tabs)/map.tsx
@@ -1,47 +1,174 @@
-import { PROVIDER_GOOGLE } from 'react-native-maps';
+// import { PROVIDER_GOOGLE } from 'react-native-maps';
+// import MapView, { Marker } from 'react-native-maps';
+// import { StyleSheet, View, TouchableOpacity, Image } from 'react-native';
+// import { useRouter } from 'expo-router';
+
+// export default function Map() {
+//   const router = useRouter();
+  
+//   return (
+//     <View style={styles.container}>
+//       <MapView
+//         style={styles.map}
+//         initialRegion={{
+//           latitude: 34.0689,  // UCLA latitude
+//           longitude: -118.4452, // UCLA longitude
+//           latitudeDelta: 0.0922,
+//           longitudeDelta: 0.0421,
+//         }}
+//       >
+//         <Marker
+//           coordinate={{ latitude: 34.0689, longitude: -118.4452 }}
+//           title="UCLA"
+//           description="University of California, Los Angeles"
+//         />
+//       </MapView>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#084141',
+//   },
+//   map: {
+//     width: '100%',
+//     height: '100%',
+//     opacity: 0.85,
+//   },
+//   button: {
+//     position: 'absolute',
+//     bottom: 20,
+//     right: 20,
+//     width: 50,
+//     height: 50,
+//   },
+// });
+
+import { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Dimensions, TextInput } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, View, TouchableOpacity, Image } from 'react-native';
-import { useRouter } from 'expo-router';
+import * as Location from 'expo-location';
+// import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { Ionicons } from "@expo/vector-icons"; // Import icons
 
 export default function Map() {
-  const router = useRouter();
-  
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [search, setSearch] = useState<string>('');
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
+
+  if (errorMsg) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>{errorMsg}</Text>
+      </View>
+    );
+  }
+
   return (
+    
     <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 34.0689,  // UCLA latitude
-          longitude: -118.4452, // UCLA longitude
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        <Marker
-          coordinate={{ latitude: 34.0689, longitude: -118.4452 }}
-          title="UCLA"
-          description="University of California, Los Angeles"
-        />
-      </MapView>
+      {/* <StatusBar hidden /> */}
+      <View style={styles.searchBarContainer}>
+        <Ionicons name="search-outline" size={20} color="#ffffff"/>
+          <TextInput
+            style={styles.searchBar}
+            placeholder="Search"
+            placeholderTextColor="#B6B3AC"
+            value={search}
+            onChangeText={setSearch}
+          />
+      </View>
+      
+      {location ? (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.03, // Zoom level
+            longitudeDelta: 0.03,
+          }}
+          showsUserLocation={true}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            title="Current Location"
+          />
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude + 0.001,
+              longitude: location.coords.longitude + 0.001,
+            }}
+            title="Another location"
+          />
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude - 0.002,
+              longitude: location.coords.longitude - 0.002,
+            }}
+            title="Another location"
+          />
+        </MapView>
+      ) : (
+        <Text style={styles.paragraph}>Fetching location...</Text>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
+    ...StyleSheet.absoluteFillObject,
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#184040',
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  searchBar: {
+    flex: 1,
+    color: '#ffffff',
+    fontSize: 15,
+    marginLeft: 5,
+  },
+  searchBarContainer: {
+    position: 'absolute',
+    top: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
     backgroundColor: '#084141',
+    borderRadius: 28,
+    width: '90%',
+    padding: 10,
+    zIndex: 1,
   },
   map: {
-    width: '100%',
-    height: '100%',
-    opacity: 0.85,
-  },
-  button: {
-    position: 'absolute',
-    bottom: 20,
-    right: 20,
-    width: 50,
-    height: 50,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
